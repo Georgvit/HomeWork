b6ea2539f0538aff9a3d02f345b5f8ca6212d162
Georgvit <ifreyd@mail.ru>
2022-09-13 23:27:32 +0300

    метод создания массив

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿//Метод создания массива с пользовательским вводом элементов
+void Array(string[] massif)
+{
+    for (int countElement = 0; countElement < massif.Length; countElement++)
+    {
+        Console.WriteLine("Введите элеммент массива:");
+        massif[countElement] = Console.ReadLine();
+
+    }
+
+
+}
