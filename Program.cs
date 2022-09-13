f9efbd58cedf05b35a21ef4286172cc19b29de21
Georgvit <ifreyd@mail.ru>
2022-09-13 23:30:21 +0300

    метод создания нового массив

--- Program.cs
+++ Program.cs
@@ -10,3 +10,35 @@ void Array(string[] massif)
 
 
 }
+
+//Метод создания нового строкового массива с заданной длиной элемента 
+void CreatingNewArray(string[] massif)
+{
+    Console.WriteLine("Введите количество элемментов в строке для формирования нового массива:");
+    int lengthString = Convert.ToInt32(Console.ReadLine());
+
+    //Новый массив
+    string[] newMassif = new string[massif.Length];
+
+    //Счетчик элементов в новом массиве
+    int countNewMassif = 0;
+
+    //Перебор оригинального массива, и присвоение выбранных элемнтов новому массиву
+    for (int countMassif = 0; countMassif < massif.Length; countMassif++)
+    {
+
+        if (massif[countMassif].Length <= lengthString)
+        {
+            newMassif[countNewMassif] = massif[countMassif];
+            countNewMassif++;
+
+        }
+
+
+    }
+
+    //Вывод  сформированного массива
+    Console.WriteLine("\nНовый массив");
+    Console.WriteLine(string.Join(" ", newMassif));
+}
+
